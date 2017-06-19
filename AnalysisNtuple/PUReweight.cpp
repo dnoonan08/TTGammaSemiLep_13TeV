661d4b2c6a0b4d6635bdd203e8607e10f61272f9
Danny Noonan <dnoonan@cern.ch>
2017-06-19 09:34:27 -0500

    fix reweighting bug caused by different bin widths

--- AnalysisNtuple/PUReweight.cpp
+++ AnalysisNtuple/PUReweight.cpp
@@ -1,6 +1,6 @@
 #include"PUReweight.h"
 #include<iostream>
-#include<TCanvas.h> 
+//#include<TCanvas.h> 
 PUReweight::PUReweight(int nFiles, char** fileNames, std::string PUfilename){
 	PUweightSum = 0.0;
 	events = 0;
@@ -9,19 +9,23 @@ PUReweight::PUReweight(int nFiles, char** fileNames, std::string PUfilename){
 	PUweightHist->SetDirectory(0);
 	pileupFile->Close();
 	TH1D* PUbackup;
-	if(PUweightHist->GetNbinsX() != 1000){
+
+	if(PUweightHist->GetNbinsX() != 200){
 		std::cout << "Wrong number of bins in the pileup histogram" << std::endl;
-		PUbackup = new TH1D("pileup_new","pileup_new",1000,0,200);
+		PUbackup = new TH1D("pileup_new","pileup_new",200,0,200);
 		for(int ibin=1; ibin <= PUweightHist->GetNbinsX(); ibin++){
 			PUbackup->SetBinContent(ibin, PUweightHist->GetBinContent(ibin));
 			// assuming the same scale
 		}
 		PUweightHist = PUbackup;
 	}
+
 	double PUweightInt = PUweightHist->Integral();
+
+
 	TH1F* mcPU = NULL;
 	for(int nmcfile = 0; nmcfile<nFiles; nmcfile++){
-		std::cout << "reading file " << std::string(fileNames[nmcfile]) << std::endl;
+		//		std::cout << "reading file " << std::string(fileNames[nmcfile]) << std::endl;
 		TFile* mcFile = TFile::Open(fileNames[nmcfile],"READ");
 		if(!(mcFile->Get("ggNtuplizer/hPUTrue"))) {
 			std::cout << "no hPU histogram here!" << std::endl;
@@ -34,14 +38,23 @@ PUReweight::PUReweight(int nFiles, char** fileNames, std::string PUfilename){
 		mcPU->SetDirectory(0);
 		mcFile->Close();
 	}
-	TCanvas *c1 = new TCanvas("c1","A Simple Graph Example",1000,500);
-        c1->SetFillColor(0);
-        c1->SetGrid();
+	// TCanvas *c1 = new TCanvas("c1","A Simple Graph Example",1000,500);
+	// c1->SetFillColor(0);
+	// c1->SetGrid();
+
+	// mcPU for some reason seems to be 1000 bins, rebin by 1000/200 (factor of 5) to get same binning in both to allow the scaling to work
+
+	if (mcPU->GetNbinsX()!=PUweightHist->GetNbinsX()){
+		if (mcPU->GetNbinsX()>PUweightHist->GetNbinsX()){
+			mcPU->Rebin(mcPU->GetNbinsX()/PUweightHist->GetNbinsX());
+		}
+	}
+
 	mcPU->Scale(1.0/mcPU->Integral());
 	PUweightHist->Divide(mcPU);
 	PUweightHist->Scale(1.0/PUweightInt);
-	PUweightHist->Draw();
-	c1->SaveAs("PUReweight.png");	
+	// PUweightHist->Draw();
+	// c1->SaveAs("PUReweight.png");	
 	delete mcPU;
 }
 
