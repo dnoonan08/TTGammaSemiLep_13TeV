f7c884c6fa8532de0c38c5b388598d477c9924e6
Danny Noonan <dnoonan@cern.ch>
2017-07-10 19:06:33 -0500

    update way histograms are made/filled to speed it up, and add in QCD

--- Plotting/makeHistograms.py
+++ Plotting/makeHistograms.py
@@ -3,17 +3,35 @@ import sys
 from sampleInformation import *
 import os
 
+gROOT.SetBatch(True)
 
 finalState = "Mu"
 
+
+extraCutsTight       = "(passPresel_Mu && nJet>=4 && nBJet>=2)*"
+extraPhotonCutsTight = "(passPresel_Mu && nJet>=4 && nBJet>=2 && phoMediumID)*"
+
+extraCuts       = "(passPresel_Mu)*"
+extraPhotonCuts = "(passPresel_Mu && phoMediumID)*"
+
 if finalState=="Mu":
     sampleList[-1] = "DataMu"
-    analysisNtupleLocation = "/uscms_data/d2/dnoonan/13TeV_TTGamma/Ntuples/muons/"
-    outputhistName = "histograms/mu/testhists.root"
+    sampleList[-2] = "QCDMu"
+    analysisNtupleLocation = "root://cmseos.fnal.gov//store/user/dnoonan/13TeV_AnalysisNtuples/muons/"
+    outputhistName = "histograms/mu/hists.root"
 if finalState=="Ele":
     sampleList[-1] = "DataEle"
+    sampleList[-2] = "QCDEle"
     analysisNtupleLocation = "/uscms_data/d2/dnoonan/13TeV_TTGamma/Ntuples/electrons/"
-    outputhistName = "histograms/ele/testhists.root"
+    outputhistName = "histograms/ele/hists.root"
+
+
+if "Tight" in sys.argv:
+    print "Tight Select"
+    extraCuts = extraCutsTight
+    extraPhotonCuts = extraPhotonCutsTight
+    sys.argv.remove("Tight")
+    outputhistName = outputhistName.replace("hists.root","hists_tight.root")
 
 
 sample = sys.argv[-1]
@@ -23,124 +41,86 @@ if not sample in sampleList:
     print sampleList
     sys.exit()
 
-preselhistogramInformation = {"jet1Pt" :[280,20,300],
-                              "jet2Pt" :[280,20,300],
-                              "jet3Pt" :[280,20,300],
-                              "Njet"   :[8,2,10],
-                              "M3"     :[100,0,400],
-                              "nVtx"   :[40,0,40],
-                              }
+# preselhistogramInformation = {"jet1Pt" :[980,20,1000],
+#                               "jet2Pt" :[280,20,600],
+#                               "jet3Pt" :[280,20,300],
+#                               "Njet"   :[8,2,10],
+#                               "Nbjet"   :[8,1,10],
+#                               "M3"     :[300,0,600],
+#                               "nVtx"   :[40,0,40],
+#                               }
 
-photonhistogramInformation = {"photon1Et"      :[40,0,200],
-                              "photon1Phi"     :[100,-3.15,3.15],
-                              "photon1Eta"     :[100,-2.5,2.5],
-                              "photon1Iso"     :[100,0.,10.],
-                              "dRPhotonLepton" :[100,0.,5.],
-                              "dRPhotonJet"    :[100,0.,5.],
-                              "M3"             :[100,0,400],
-                              }
+# photonhistogramInformation = {"photon1Et"      :[40,0,200],
+#                               "photon1Phi"     :[100,-3.15,3.15],
+#                               "photon1Eta"     :[100,-2.5,2.5],
+#                               "photon1Iso"     :[100,0.,10.],
+#                               "dRPhotonLepton" :[100,0.,5.],
+#                               "dRPhotonJet"    :[100,0.,5.],
+#                               "M3"             :[100,0,400],
+#                               }
 
 
-preselhistogramList = {}
+tree = TChain("AnalysisTree")
+fileList = samples[sample][0]
+for fileName in fileList:
+    tree.Add("%s/%s"%(analysisNtupleLocation,fileName))
 
-for histo in preselhistogramInformation:
-    histInfo = preselhistogramInformation[histo]
-    preselhistogramList[histo] = {}
-    preselhistogramList[histo][sample] = TH1F("preSel_%s_%s"%(histo,sample),"preSel_%s_%s"%(histo,sample),histInfo[0],histInfo[1],histInfo[2])
-    preselhistogramList[histo][sample].SetFillColor(samples[sample][1])
+print sample
 
-    if samples[sample][-1]==isData:
-        preselhistogramList[histo][sample].SetMarkerStyle(20)
-        preselhistogramList[histo][sample].SetLineWidth(0)
+print "Number of events:", tree.GetEntries()
 
-phoselhistogramList = {}
+# presel_jet1Pt = TH1F("presel_jet1Pt_%s"%(sample),"presel_jet1Pt_%s"%(sample),1000,0,1000)
+# presel_jet2Pt = TH1F("presel_jet2Pt_%s"%(sample),"presel_jet2Pt_%s"%(sample),1000,0,1000)
+# presel_jet3Pt = TH1F("presel_jet3Pt_%s"%(sample),"presel_jet3Pt_%s"%(sample),1000,0,1000)
+# presel_Njet   = TH1F("presel_Njet_%s"%(sample),"presel_Njet_%s"%(sample),15,0,15)
+# presel_Nbjet  = TH1F("presel_Nbjet_%s"%(sample),"presel_Nbjet_%s"%(sample),10,0,10)
+# presel_M3     = TH1F("presel_M3_%s"%(sample),"presel_M3_%s"%(sample),300,0,600)
+# presel_nVtx   = TH1F("presel_nVtx_%s"%(sample),"presel_nVtx_%s"%(sample),50,0,50)
+# presel_nVtxup = TH1F("presel_nVtxup_%s"%(sample),"presel_nVtxup_%s"%(sample),50,0,50)
+# presel_nVtxdo = 
 
-for histo in photonhistogramInformation:
-    histInfo = photonhistogramInformation[histo]
-    phoselhistogramList[histo] = {}
-    phoselhistogramList[histo][sample] = TH1F("phoSel_%s_%s"%(histo,sample),"phoSel_%s_%s"%(histo,sample),histInfo[0],histInfo[1],histInfo[2])
-    phoselhistogramList[histo][sample].SetFillColor(samples[sample][1])
 
-    if samples[sample][-1]==isData:
-        phoselhistogramList[histo][sample].SetMarkerStyle(20)
-        phoselhistogramList[histo][sample].SetLineWidth(0)
+
+# 
+histogramInfo = [ ["jetPt[0]" , "presel_jet1Pt"   ,    [1000,0,1000],  extraCuts      , ""],
+                  ["jetPt[1]" , "presel_jet2Pt"   ,    [1000,0,1000],  extraCuts      , ""],
+                  ["jetPt[2]" , "presel_jet3Pt"   ,    [1000,0,1000],  extraCuts      , ""],
+                  ["nJet"     , "presel_Njet"     ,        [15,0,15],  extraCuts      , ""],
+                  ["nBJet"    , "presel_Nbjet"    ,        [10,0,10],  extraCuts      , ""],
+                  ["M3"       , "presel_M3"       ,      [300,0,600],  extraCuts      , ""],
+                  ["nVtx"     , "presel_nVtx"     ,        [50,0,50],  extraCuts      , ""],
+                  ["nVtx"     , "presel_nVtxup"   ,        [50,0,50],  extraCuts      , "%sevtWeight*PUweight_Up*muEffWeight*eleEffWeight*btagWeight"%extraCuts],
+                  ["nVtx"     , "presel_nVtxdo"   ,        [50,0,50],  extraCuts      , "%sevtWeight*PUweight_Do*muEffWeight*eleEffWeight*btagWeight"%extraCuts],
+                  ["phoEt"    , "phosel_photonEt" ,      [300,0,300],  extraPhotonCuts, ""],
+                  ["phoEta"   , "phosel_photonEta",   [100,-2.5,2.5],  extraPhotonCuts, ""],
+                  ["phoPhi"   , "phosel_photonPhi", [100,-3.15,3.15],  extraPhotonCuts, ""],
+                  ["M3"        , "phosel_M3"      , [100,-3.15,3.15],  extraPhotonCuts, ""],
+                  ]
+
+
+histograms = []
+for h_Info in histogramInfo:
+    print "filling", h_Info[1], sample
+    evtWeight = ""
+    histograms.append(TH1F("%s_%s"%(h_Info[1],sample),"%s_%s"%(h_Info[1],sample),h_Info[2][0],h_Info[2][1],h_Info[2][2]))
+    if h_Info[-1]=="":
+        evtWeight = "%sevtWeight*PUweight*muEffWeight*eleEffWeight*btagWeight"%h_Info[3]
+    else:
+        evtWeight = h_Info[-1]
         
+    if "Data" in sample:
+        evtWeight = h_Info[3]
 
+    if evtWeight[-1]=="*":
+        evtWeight= evtWeight[:-1]
+    tree.Draw("%s>>%s_%s"%(h_Info[0],h_Info[1],sample),evtWeight)
 
-def fillPreselHistograms(preselhistogramList,sample,event,fillWeight):
-    preselhistogramList['M3'][sample].Fill(event.M3,fillWeight)
-    preselhistogramList['jet1Pt'][sample].Fill(event.jetPt[0],fillWeight)
-    preselhistogramList['jet2Pt'][sample].Fill(event.jetPt[1],fillWeight)
-    preselhistogramList['jet3Pt'][sample].Fill(event.jetPt[2],fillWeight)
-    preselhistogramList['Njet'][sample].Fill(event.nJet,fillWeight)
-    preselhistogramList['nVtx'][sample].Fill(event.nVtx,fillWeight)
-
-
-def fillPhoselHistograms(phoselhistogramList,sample,event,fillWeight):
-    for i in range(len(event.phoEt)):
-        if event.phoMediumID[i]:
-            phoselhistogramList['M3'][sample].Fill(event.M3,fillWeight)
-            phoselhistogramList['photon1Et'][sample].Fill(event.phoEt[i],fillWeight)
-            phoselhistogramList['photon1Eta'][sample].Fill(event.phoEta[i],fillWeight)
-            phoselhistogramList['photon1Phi'][sample].Fill(event.phoPhi[i],fillWeight)
-            phoselhistogramList['photon1Iso'][sample].Fill(event.phoPFChIso[i]+event.phoPFNeuIso[i]+event.phoPFPhoIso[i],fillWeight)
-            for dR in event.dRPhotonLepton:
-                phoselhistogramList['dRPhotonLepton'][sample].Fill(dR,fillWeight)
-            for dR in event.dRPhotonJet:
-                phoselhistogramList['dRPhotonJet'][sample].Fill(dR,fillWeight)
-            break
-
-
-
-if sample in sampleList:
-    tree = TChain("AnalysisTree")
-    fileList = samples[sample][0]
-    for fileName in fileList:
-        tree.Add("%s/%s"%(analysisNtupleLocation,fileName))
-
-    print sample
-
-    print "Number of events:", tree.GetEntries()
-
-    i = 0.    
-    N = tree.GetEntries()
-    k = 50
-    
-    for event in tree:
-        if i%(int(N/k))==0:            
-            j = int(i/(int(N/k)))
-
-            sys.stdout.write("[%s%s] %3i%%" % ("="*j," " * (k-j),int(i*100/N)))
-            sys.stdout.flush()
-            sys.stdout.write("\b"*(k+8))
-        i += 1.
-        if not event.passPresel_Mu:# or event.nJet < 4 or event.nBJet<2:
-            continue
-
-        fillWeight = 1.        
-
-        if samples[sample][-1]==isMC:
-            fillWeight *= event.evtWeight
-            fillWeight *= event.PUweight
-            fillWeight *= event.btagWeight
-            fillWeight *= event.muEffWeight
-            fillWeight *= event.eleEffWeight
-
-        fillPreselHistograms(preselhistogramList,sample,event,fillWeight)
-        
-        if event.passAll_Mu:
-            fillPhoselHistograms(phoselhistogramList,sample,event,fillWeight)
-
-    sys.stdout.write("[%s] 100%%" % ("="*50))
-    print
-    tree.Clear()
 
 outputFile = TFile(outputhistName,"update")
 outputFile.rmdir(sample)
 outputFile.mkdir(sample)
 outputFile.cd(sample)
-for hist in preselhistogramList:
-    preselhistogramList[hist][sample].Write()
-for hist in phoselhistogramList:
-    phoselhistogramList[hist][sample].Write()
+for h in histograms:
+    h.Write()
+
 outputFile.Close()
--- Plotting/makePlots.py
+++ Plotting/makePlots.py
@@ -1,69 +1,175 @@
 from ROOT import *
+import os
 
 from sampleInformation import *
 
 gROOT.SetBatch(True)
 
+import CMS_lumi
+
+from Style import *
+thestyle = Style()
+
+HasCMSStyle = False
+style = None
+if os.path.isfile('tdrstyle.C'):
+    ROOT.gROOT.ProcessLine('.L tdrstyle.C')
+    ROOT.setTDRStyle()
+    print "Found tdrstyle.C file, using this style."
+    HasCMSStyle = True
+    if os.path.isfile('CMSTopStyle.cc'):
+        gROOT.ProcessLine('.L CMSTopStyle.cc+')
+        style = CMSTopStyle()
+        style.setupICHEPv1()
+        print "Found CMSTopStyle.cc file, use TOP style if requested in xml file."
+if not HasCMSStyle:
+    print "Using default style defined in cuy package."
+    thestyle.SetStyle()
+
+ROOT.gROOT.ForceStyle()
+
+
 stackList = sampleList[:-1]
 
-stackList.remove("WJets")
+# stackList.remove("WJets")
 stackList.reverse()
 
 _file  = TFile("histograms/mu/testhists.root")
 
-preselhistograms = ["jet1Pt" ,
-                    "jet2Pt" ,
-                    "jet3Pt" ,
-                    "Njet"   ,
-                    "M3"     ,
-                    "nVtx"    ,
-                    ]
+YesLog = True
+NoLog=False
 
-phoselhistograms = ["photon1Et"     ,     
-                    "photon1Phi"    ,    
-                    "photon1Eta"    ,    
-                    "dRPhotonLepton",
-                    "dRPhotonJet"   ,   
-                    "M3"            ,            
-                    ]
+preselhistograms = {"jet1Pt" : ["Leading Jet Pt (GeV)",  YesLog],
+                    "jet2Pt" : ["Second Jet Pt (GeV)",  YesLog],
+                    "jet3Pt" : ["Third Jet Pt (GeV)",  YesLog],
+                    "Njet"   : ["N Jets",  NoLog],
+                    "Nbjet"  : ["N B-Jets",  NoLog],
+                    "M3"     : ["M_{3} (GeV)",  YesLog],
+                    "nVtx"   : ["N Vtx",   NoLog],
+                    "nVtxup"   : ["N Vtx",   NoLog],
+                    "nVtxdo"   : ["N Vtx",   NoLog],
+                    }
 
-c1 = TCanvas("c1","c1",1200,800)
-c1.cd()
+phoselhistograms = {"photonEt"      : ["Photon Et (GeV)"  , NoLog],     
+                    "photonPhi"     : ["Photon Phi (GeV)" , NoLog],    
+                    "photonEta"     : ["Photon Eta (GeV)" , NoLog],    
+                    # "dRPhotonLepton" : ["dR(Photon,Lepton)", NoLog],
+                    # "dRPhotonJet"    : ["dR(Photon,Jet)"   , NoLog],   
+                    # "M3"             : ["M_{3} (GeV)"         , NoLog],            
+                    }
+
+
+
+H = 600;
+W = 800;
+
+canvas = TCanvas('c1','c1',W,H)
+
+# references for T, B, L, R                                                                                                             
+T = 0.08*H
+B = 0.12*H
+L = 0.12*W
+R = 0.04*W
+canvas.SetFillColor(0)
+canvas.SetBorderMode(0)
+canvas.SetFrameFillStyle(0)
+canvas.SetFrameBorderMode(0)
+canvas.SetLeftMargin( L/W )
+canvas.SetRightMargin( R/W )
+canvas.SetTopMargin( T/H )
+canvas.SetBottomMargin( B/H )
+canvas.SetTickx(0)
+
+canvas.cd()
+
+legList = {'TTGamma': [kAzure+3, 't#bar{t}+#gamma'],
+          'TTJets': [kRed+1, 't#bar{t}+jets'],
+          'TTV': [kRed+1, 't#bar{t}+V'],
+          'Vgamma': [kGray, 'W/Z+#gamma'],
+          'SingleTop': [kMagenta, 'Single t'],
+          'WJets': [kGreen -3, 'W+jets'],
+          'ZJets': [kGreen -3, 'Z+jets'],
+          'QCD': [kYellow, 'Multijet'],
+          }
+
+mcList = {'TTGamma': [kOrange],
+          'TTbar': [kRed+1],
+          'TTV': [kRed-7],
+          'SingleTop': [kOrange-3],
+          'WGamma': [kBlue-4],
+          'ZGamma': [kBlue-4],
+          'WJets': [kCyan-3],
+          'ZJets': [kCyan-3],
+
+          'QCD': [kGreen+3],
+          }
+
+legend = TLegend(0.71, 1-T/H-0.01 - 0.06*(len(legList)+1), 1-R/W-0.01, 1-(T/H)-0.01)
+
+legend.SetBorderSize(0)
+legend.SetFillColor(0)
+
+histName = preselhistograms.keys()[0]
+legList = stackList[:]
+legList.reverse()
+dataHist = _file.Get("DataMu/presel_%s_DataMu"%(histName))
+legend.AddEntry(dataHist, "Data", 'pe')
+
+for sample in legList:
+    hist = _file.Get("%s/presel_%s_%s"%(sample,histName,sample))
+    hist.SetFillColor(mcList[sample][0])
+    hist.SetLineColor(mcList[sample][0])
+    legend.AddEntry(hist,sample,'f')
+
 for histName in preselhistograms:
     stack = THStack("presel_%s"%histName,"presel_%s"%histName)
+
     for sample in stackList:
-        hist = _file.Get("%s/preSel_%s_%s"%(sample,histName,sample)).Clone(sample)
+        hist = _file.Get("%s/presel_%s_%s"%(sample,histName,sample)).Clone(sample)
+        
         # print hist
         # print hist.Integral()
+        hist.SetFillColor(mcList[sample][0])
+        hist.SetLineColor(mcList[sample][0])
         stack.Add(hist)
         # print stack.GetStack().Last().Integral()
-    dataHist = _file.Get("DataMu/preSel_%s_DataMu"%(histName))
 
+
+    dataHist = _file.Get("DataMu/presel_%s_DataMu"%(histName))
+    
     print dataHist
+    canvas.SetLogy(preselhistograms[histName][-1])
 
     stack.SetMaximum(1.05*max(dataHist.GetMaximum(),stack.GetMaximum()))
-    stack.Draw("hist,e0")
-    dataHist.Draw("e,same")
-    
-    c1.SaveAs("plots/presel_mu_%s.pdf"%histName)
+    stack.Draw("HIST")
+    stack.GetHistogram().GetXaxis().SetTitle(preselhistograms[histName][0])
+    dataHist.Draw("e,X0,same")
+    legend.Draw("same")
+    canvas.SaveAs("plots/presel_mu_%s.pdf"%histName)
+    canvas.SetLogy(0)
 
 
 
 for histName in phoselhistograms:
     stack = THStack("phosel_%s"%histName,"phosel_%s"%histName)
     for sample in stackList:
-        hist = _file.Get("%s/phoSel_%s_%s"%(sample,histName,sample)).Clone(sample)
+        hist = _file.Get("%s/phosel_%s_%s"%(sample,histName,sample)).Clone(sample)
         # print hist
         # print hist.Integral()
+        hist.SetFillColor(mcList[sample][0])
+        hist.SetLineColor(mcList[sample][0])
         stack.Add(hist)
         # print stack.GetStack().Last().Integral()
-    dataHist = _file.Get("DataMu/phoSel_%s_DataMu"%(histName))
+    dataHist = _file.Get("DataMu/phosel_%s_DataMu"%(histName))
 
     print dataHist
+    canvas.SetLogy(phoselhistograms[histName][-1])
 
     stack.SetMaximum(1.05*max(dataHist.GetMaximum(),stack.GetMaximum()))
-    stack.Draw("hist,e0")
-    dataHist.Draw("e,same")
-    
-    c1.SaveAs("plots/phosel_mu_%s.pdf"%histName)
+    stack.Draw("hist")
+    stack.GetHistogram().GetXaxis().SetTitle(phoselhistograms[histName][0])
+    dataHist.Draw("e,X0,same")
+    legend.Draw("same")
+
+    canvas.SaveAs("plots/phosel_mu_%s.pdf"%histName)
 
