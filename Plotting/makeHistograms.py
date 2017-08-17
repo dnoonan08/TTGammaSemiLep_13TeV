723180f44a854ad02de930ccdab536ad0f0dc8b9
Danny Noonan <dnoonan@cern.ch>
2017-08-17 15:06:17 -0500

    Add jet/bjet region selections

--- Plotting/makeHistograms.py
+++ Plotting/makeHistograms.py
@@ -7,12 +7,26 @@ gROOT.SetBatch(True)
 
 finalState = "Mu"
 
+nBJets = 1
+
+btagWeightCategory = ["1","(1-btagWeight[0])","(btagWeight[2])","(btagWeight[1])"]
+
+
 
 extraCutsTight       = "(passPresel_Mu && nJet>=4 && nBJet>=2)*"
-extraPhotonCutsTight = "(passPresel_Mu && nJet>=4 && nBJet>=2 && phoMediumID)*"
+extraPhotonCutsTight = "(passPresel_Mu && nJet>=4 && nBJet>=2 && %s)*"
 
-extraCuts       = "(passPresel_Mu)*"
-extraPhotonCuts = "(passPresel_Mu && phoMediumID)*"
+extraCutsLoose       = "(passPresel_Mu && nJet>=2 && nBJet>=1)*"
+extraPhotonCutsLoose = "(passPresel_Mu && nJet>=2 && nBJet>=1 && %s)*"
+
+extraCutsLooseCR       = "(passPresel_Mu && nJet==2 && nBJet>=1)*"
+extraPhotonCutsLooseCR = "(passPresel_Mu && nJet==2 && nBJet>=1 && %s)*"
+
+extraCuts            = "(passPresel_Mu && nJet>=3 && nBJet>=1)*"
+extraPhotonCuts      = "(passPresel_Mu && nJet>=3 && nBJet>=1 && %s)*"
+
+# extraCutsTight       = "(passPresel_Mu && nJet>=3 && nBJet>=2)*"
+# extraPhotonCutsTight = "(passPresel_Mu && nJet>=3 && nBJet>=2 && %s)*"
 
 if finalState=="Mu":
     sampleList[-1] = "DataMu"
@@ -26,12 +40,39 @@ if finalState=="Ele":
     outputhistName = "histograms/ele/hists.root"
 
 
+
+
+weights = "evtWeight*PUweight*muEffWeight*eleEffWeight*%s"%btagWeightCategory[nBJets]
+
 if "Tight" in sys.argv:
     print "Tight Select"
+    nBJets = 2
+    weights = "evtWeight*PUweight*muEffWeight*eleEffWeight*btagWeight[2]"
     extraCuts = extraCutsTight
     extraPhotonCuts = extraPhotonCutsTight
     sys.argv.remove("Tight")
     outputhistName = outputhistName.replace("hists.root","hists_tight.root")
+
+if "Loose" in sys.argv:
+    print "Loose Select"
+    nBJets = 1
+    weights = "evtWeight*PUweight*muEffWeight*eleEffWeight*(btagWeight[1]+btagWeight[2])"
+    extraCuts = extraCutsLoose
+    extraPhotonCuts = extraPhotonCutsLoose
+    sys.argv.remove("Loose")
+    outputhistName = outputhistName.replace("hists.root","hists_loose.root")
+
+if "LooseCR" in sys.argv:
+    print "Loose Control Region Select"
+    nBJets = 1
+    weights = "evtWeight*PUweight*muEffWeight*eleEffWeight*(btagWeight[1]+btagWeight[2])"
+    extraCuts = extraCutsLooseCR
+    extraPhotonCuts = extraPhotonCutsLooseCR
+    sys.argv.remove("LooseCR")
+    outputhistName = outputhistName.replace("hists.root","hists_2jControlRegion.root")
+
+
+
 
 
 sample = sys.argv[-1]
@@ -41,23 +82,6 @@ if not sample in sampleList:
     print sampleList
     sys.exit()
 
-# preselhistogramInformation = {"jet1Pt" :[980,20,1000],
-#                               "jet2Pt" :[280,20,600],
-#                               "jet3Pt" :[280,20,300],
-#                               "Njet"   :[8,2,10],
-#                               "Nbjet"   :[8,1,10],
-#                               "M3"     :[300,0,600],
-#                               "nVtx"   :[40,0,40],
-#                               }
-
-# photonhistogramInformation = {"photon1Et"      :[40,0,200],
-#                               "photon1Phi"     :[100,-3.15,3.15],
-#                               "photon1Eta"     :[100,-2.5,2.5],
-#                               "photon1Iso"     :[100,0.,10.],
-#                               "dRPhotonLepton" :[100,0.,5.],
-#                               "dRPhotonJet"    :[100,0.,5.],
-#                               "M3"             :[100,0,400],
-#                               }
 
 
 tree = TChain("AnalysisTree")
@@ -69,32 +93,42 @@ print sample
 
 print "Number of events:", tree.GetEntries()
 
-# presel_jet1Pt = TH1F("presel_jet1Pt_%s"%(sample),"presel_jet1Pt_%s"%(sample),1000,0,1000)
-# presel_jet2Pt = TH1F("presel_jet2Pt_%s"%(sample),"presel_jet2Pt_%s"%(sample),1000,0,1000)
-# presel_jet3Pt = TH1F("presel_jet3Pt_%s"%(sample),"presel_jet3Pt_%s"%(sample),1000,0,1000)
-# presel_Njet   = TH1F("presel_Njet_%s"%(sample),"presel_Njet_%s"%(sample),15,0,15)
-# presel_Nbjet  = TH1F("presel_Nbjet_%s"%(sample),"presel_Nbjet_%s"%(sample),10,0,10)
-# presel_M3     = TH1F("presel_M3_%s"%(sample),"presel_M3_%s"%(sample),300,0,600)
-# presel_nVtx   = TH1F("presel_nVtx_%s"%(sample),"presel_nVtx_%s"%(sample),50,0,50)
-# presel_nVtxup = TH1F("presel_nVtxup_%s"%(sample),"presel_nVtxup_%s"%(sample),50,0,50)
-# presel_nVtxdo = 
-
-
-
 # 
-histogramInfo = [ ["jetPt[0]" , "presel_jet1Pt"   ,    [1000,0,1000],  extraCuts      , ""],
-                  ["jetPt[1]" , "presel_jet2Pt"   ,    [1000,0,1000],  extraCuts      , ""],
-                  ["jetPt[2]" , "presel_jet3Pt"   ,    [1000,0,1000],  extraCuts      , ""],
-                  ["nJet"     , "presel_Njet"     ,        [15,0,15],  extraCuts      , ""],
-                  ["nBJet"    , "presel_Nbjet"    ,        [10,0,10],  extraCuts      , ""],
-                  ["M3"       , "presel_M3"       ,      [300,0,600],  extraCuts      , ""],
-                  ["nVtx"     , "presel_nVtx"     ,        [50,0,50],  extraCuts      , ""],
-                  ["nVtx"     , "presel_nVtxup"   ,        [50,0,50],  extraCuts      , "%sevtWeight*PUweight_Up*muEffWeight*eleEffWeight*btagWeight"%extraCuts],
-                  ["nVtx"     , "presel_nVtxdo"   ,        [50,0,50],  extraCuts      , "%sevtWeight*PUweight_Do*muEffWeight*eleEffWeight*btagWeight"%extraCuts],
-                  ["phoEt"    , "phosel_photonEt" ,      [300,0,300],  extraPhotonCuts, ""],
-                  ["phoEta"   , "phosel_photonEta",   [100,-2.5,2.5],  extraPhotonCuts, ""],
-                  ["phoPhi"   , "phosel_photonPhi", [100,-3.15,3.15],  extraPhotonCuts, ""],
-                  ["M3"        , "phosel_M3"      , [100,-3.15,3.15],  extraPhotonCuts, ""],
+histogramInfo = [ ["jetPt[0]" , "presel_jet1Pt"   ,    [1000,0,1000], extraCuts      , ""],
+                  ["jetPt[1]" , "presel_jet2Pt"   ,      [600,0,600], extraCuts      , ""],
+                  ["jetPt[2]" , "presel_jet3Pt"   ,      [600,0,600], extraCuts      , ""],
+                  ["nJet"     , "presel_Njet"     ,        [15,0,15], extraCuts      , ""],
+                  ["nBJet"    , "presel_Nbjet"    ,        [10,0,10], extraCuts      , ""],
+                  ["muPt[0]"  , "presel_muPt"     ,      [600,0,600], extraCuts      , ""],
+                  ["muEta[0]" , "presel_muEta"    ,   [100,-2.4,2.4], extraCuts      , ""],
+                  ["muPhi[0]" , "presel_muPhi"    , [100,-3.15,3.15], extraCuts      , ""],
+                  ["M3"       , "presel_M3"       ,      [300,0,600], extraCuts      , ""],
+                  ["pfMET"    , "presel_MET"      ,      [300,0,600], extraCuts      , ""],
+                  ["pfMETPhi" , "presel_METPhi"   , [100,-3.15,3.15], extraCuts      , ""],
+                  # ["nVtx"     , "presel_nVtx"     ,        [50,0,50], extraCuts      , ""],
+                  # ["nVtx"     , "presel_nVtxup"   ,        [50,0,50], extraCuts      , "%sevtWeight*PUweight_Up*muEffWeight*eleEffWeight*%s"%(extraCuts,btagWeightCategory[nBJets])],
+                  # ["nVtx"     , "presel_nVtxdo"   ,        [50,0,50], extraCuts      , "%sevtWeight*PUweight_Do*muEffWeight*eleEffWeight*%s"%(extraCuts,btagWeightCategory[nBJets])],
+                  # ["nVtx"     , "presel_nVtxNoPU" ,        [50,0,50], extraCuts      , "%sevtWeight*muEffWeight*eleEffWeight*%s"%(extraCuts,btagWeightCategory[nBJets])],
+                  ["phoEt"      , "phosel_photonEt" ,      [300,0,300], extraPhotonCuts%("phoMediumID"), ""],
+                  ["phoEta"     , "phosel_photonEta",   [100,-2.5,2.5], extraPhotonCuts%("phoMediumID"), ""],
+                  ["dRPhotonJet"   , "phosel_dRPhotonJet"    , [200,0,5], extraPhotonCuts%("phoMediumID"), ""],
+                  ["dRPhotonLepton", "phosel_dRPhotonLepton",  [200,0,10], extraPhotonCuts%("phoMediumID"), ""],
+                  ["phoPhi"     , "phosel_photonPhi", [100,-3.15,3.15], extraPhotonCuts%("phoMediumID"), ""],
+                  ["M3"         , "phosel_M3"       ,      [120,0,600], extraPhotonCuts%("phoMediumID"), ""],
+                  ["pfMET"      , "phosel_MET"      ,      [300,0,600], extraPhotonCuts%("phoMediumID"), ""],
+                  ["nJet"       , "phosel_Njet"     ,        [15,0,15], extraPhotonCuts%("phoMediumID"), ""],
+                  ["nBJet"      , "phosel_Nbjet"    ,        [10,0,10], extraPhotonCuts%("phoMediumID"), ""],
+                  ["phoHoverE"  , "phosel_HoverE"   ,     [100,0.,.04], extraPhotonCuts%("phoMediumID"), ""],
+                  ["phoSIEIE"   , "phosel_SIEIE"    ,     [100,0.,.03], extraPhotonCuts%("phoMediumID"), ""],
+                  ["phoPFChIso" , "phosel_ChIso"    ,       [100,0,.5], extraPhotonCuts%("phoMediumID"), ""],
+                  ["phoPFNeuIso", "phosel_NeuIso"   ,       [100,0,5.], extraPhotonCuts%("phoMediumID"), ""],
+                  ["phoPFPhoIso", "phosel_PhoIso"   ,       [100,0,10], extraPhotonCuts%("phoMediumID"), ""],
+                  ["phoHoverE"  , "phosel_noCut_HoverE"   ,      [100,0.,.2], extraPhotonCuts%("phoMediumIDPassSIEIE && phoMediumIDPassChIso && phoMediumIDPassNeuIso && phoMediumIDPassPhoIso"), ""],
+                  ["phoSIEIE"   , "phosel_noCut_SIEIE"    ,     [100,0.,.08], extraPhotonCuts%("phoMediumIDPassHoverE && phoMediumIDPassChIso && phoMediumIDPassNeuIso && phoMediumIDPassPhoIso"), ""],
+                  ["phoPFChIso" , "phosel_noCut_ChIso"    ,      [200,0,100], extraPhotonCuts%("phoMediumIDPassHoverE && phoMediumIDPassSIEIE && phoMediumIDPassNeuIso && phoMediumIDPassPhoIso"), ""],
+                  ["phoPFNeuIso", "phosel_noCut_NeuIso"   ,      [200,0,100], extraPhotonCuts%("phoMediumIDPassHoverE && phoMediumIDPassSIEIE && phoMediumIDPassChIso && phoMediumIDPassPhoIso"), ""],
+                  ["phoPFPhoIso", "phosel_noCut_PhoIso"   ,      [200,0,100], extraPhotonCuts%("phoMediumIDPassHoverE && phoMediumIDPassSIEIE && phoMediumIDPassChIso && phoMediumIDPassNeuIso"), ""],
+                  ["phoPFChIso" , "phosel_AntiSIEIE_ChIso",      [200,0,100], extraPhotonCuts%("phoMediumIDPassHoverE && !phoMediumIDPassSIEIE && phoMediumIDPassNeuIso && phoMediumIDPassPhoIso"), ""],
                   ]
 
 
@@ -104,7 +138,7 @@ for h_Info in histogramInfo:
     evtWeight = ""
     histograms.append(TH1F("%s_%s"%(h_Info[1],sample),"%s_%s"%(h_Info[1],sample),h_Info[2][0],h_Info[2][1],h_Info[2][2]))
     if h_Info[-1]=="":
-        evtWeight = "%sevtWeight*PUweight*muEffWeight*eleEffWeight*btagWeight"%h_Info[3]
+        evtWeight = "%s%s"%(h_Info[3],weights)
     else:
         evtWeight = h_Info[-1]
         
--- Plotting/makePlots.py
+++ Plotting/makePlots.py
@@ -1,9 +1,98 @@
 from ROOT import *
 import os
 
+import sys
+
+
+padRatio = 0.25
+padOverlap = 0.15
+padGap = 0.01
+
+_file  = TFile("histograms/mu/hists.root")
+plotDirectory = "plots/"
+regionText = ", N_{j}#geq3, N_{b}#geq1"
+
+isTight = False
+
+if 'Tight' in sys.argv:
+    plotDirectory = "tightplots/"
+    _file  = TFile("histograms/mu/hists_tight.root")
+    regionText = ", N_{j}#geq4, N_{b}#geq2"
+if 'Loose' in sys.argv:
+    plotDirectory = "looseplots/"
+    _file  = TFile("histograms/mu/hists_loose.root")
+    regionText = ", N_{j}#geq2, N_{b}#geq1"
+if 'LooseCR' in sys.argv:
+    plotDirectory = "2j1tCRplots/"
+    _file  = TFile("histograms/mu/hists_2jControlRegion.root")
+    regionText = ", N_{j}=2, N_{b}#geq1"
+
+
+
 from sampleInformation import *
 
 gROOT.SetBatch(True)
+
+
+
+
+YesLog = True
+NoLog=False
+
+
+
+
+# Histogram Information:
+# [X-axis title, 
+#  Y-axis title,
+#  Rebinning factor,
+#  [x-min,x-max], -1 means keep as is
+#  Extra text about region
+#  log plot]
+
+
+preselhistograms = {"jet1Pt"   : ["Leading Jet Pt (GeV)", "Events", 5, [-1,-1], regionText, YesLog],
+                    "jet2Pt"   : ["Second Jet Pt (GeV)" , "Events", 5, [-1,-1], regionText, YesLog],
+                    "jet3Pt"   : ["Third Jet Pt (GeV)"  , "Events", 5, [-1,-1], regionText, YesLog],
+                    "muPt"     : ["Muon p_{T} (GeV)"    , "Events", 5, [-1,-1], regionText,  NoLog],
+                    "muEta"    : ["Muon #eta"           , "Events", 5, [-1,-1], regionText,  NoLog],
+                    "muPhi"    : ["Muon #phi"           , "Events", 5, [-1,-1], regionText,  NoLog],
+                    "Njet"     : ["N Jets"              , "Events", 1, [-1,-1], regionText,  NoLog],
+                    "Nbjet"    : ["N B-Jets"            , "Events", 1, [-1,-1], regionText,  NoLog],
+                    "M3"       : ["M_{3} (GeV)"         , "Events", 5, [-1,-1], regionText,  NoLog],
+                    "MET"      : ["MET (GeV)  "         , "Events", 5, [-1,-1], regionText,  NoLog],
+                    # "nVtx"     : ["N Vtx"               , "Events", 1, [-1,-1], regionText,  NoLog],
+                    # "nVtxup"   : ["N Vtx"               , "Events", 1, [-1,-1], regionText,  NoLog],
+                    # "nVtxdo"   : ["N Vtx"               , "Events", 1, [-1,-1], regionText,  NoLog],
+                    # "nVtxNoPU" : ["N Vtx"               , "Events", 1, [-1,-1], regionText,  NoLog],
+                    }
+
+
+
+phoselhistograms = {"photonEt"                : ["Photon Et (GeV)"            , "Events", 5, [-1,-1], regionText,  NoLog],     
+                    "photonPhi"               : ["Photon Phi (GeV)"           , "Events", 1, [-1,-1], regionText,  NoLog],    
+                    "photonEta"               : ["Photon Eta (GeV)"           , "Events", 1, [-1,-1], regionText,  NoLog],    
+                    "dRPhotonLepton"          : ["dR(Photon,Lepton)"          , "Events", 1, [-1,-1], regionText,  NoLog],
+                    "dRPhotonJet"             : ["dR(Photon,Jet)"             , "Events", 1, [-1,-1], regionText,  NoLog],   
+                    "M3"                      : ["M_{3} (GeV)"                , "Events", 5, [-1,-1], regionText,  NoLog],   
+                    "Njet"                    : ["N Jets"                     , "Events", 1, [-1,-1], regionText,  NoLog],
+                    "Nbjet"                   : ["N B-Jets"                   , "Events", 1, [-1,-1], regionText,  NoLog],
+                    "HoverE"                  : ["H over E"                   , "Events", 1, [-1,-1], regionText, YesLog],
+                    "SIEIE"                   : ["Sigma Ieta Ieta"            , "Events", 1, [-1,-1], regionText, YesLog],
+                    "ChIso"                   : ["Charged Hadron Iso (GeV)"   , "Events", 1, [-1,-1], regionText, YesLog],
+                    "NeuIso"                  : ["Neutral Hadron Iso (GeV)"   , "Events", 1, [-1,-1], regionText, YesLog],
+                    "PhoIso"                  : ["Photon Iso (GeV)"           , "Events", 1, [-1,-1], regionText, YesLog],
+                    "noCut_HoverE"            : ["H over E"                   , "Events", 1, [-1,-1], regionText, YesLog],
+                    "noCut_SIEIE"             : ["Sigma Ieta Ieta"            , "Events", 1, [-1,-1], regionText, YesLog],
+                    "noCut_ChIso"             : ["Charged Hadron Iso (GeV)"   , "Events", 1, [-1,-1], regionText, YesLog],
+                    "noCut_NeuIso"            : ["Neutral Hadron Iso (GeV)"   , "Events", 1, [-1,-1], regionText, YesLog],
+                    "noCut_PhoIso"            : ["Photon Iso (GeV)"           , "Events", 1, [-1,-1], regionText, YesLog],
+                    "AntiSIEIE_ChIso"         : ["Charged Hadron Iso (GeV)"   , "Events", 1, [-1,-1], regionText,  NoLog],
+                    "AntiHoverE_ChIso"        : ["Charged Hadron Iso (GeV)"   , "Events", 1, [-1,-1], regionText,  NoLog],
+                    "AntiHoverEOrSIEIE_ChIso" : ["Charged Hadron Iso (GeV)"   , "Events", 1, [-1,-1], regionText,  NoLog],
+                    }
+
+
 
 import CMS_lumi
 
@@ -28,36 +117,18 @@ if not HasCMSStyle:
 
 ROOT.gROOT.ForceStyle()
 
-
+sampleList[-2] = "QCDMu"
 stackList = sampleList[:-1]
 
 # stackList.remove("WJets")
 stackList.reverse()
 
-_file  = TFile("histograms/mu/testhists.root")
 
-YesLog = True
-NoLog=False
+_channelText = "#mu+jets"
 
-preselhistograms = {"jet1Pt" : ["Leading Jet Pt (GeV)",  YesLog],
-                    "jet2Pt" : ["Second Jet Pt (GeV)",  YesLog],
-                    "jet3Pt" : ["Third Jet Pt (GeV)",  YesLog],
-                    "Njet"   : ["N Jets",  NoLog],
-                    "Nbjet"  : ["N B-Jets",  NoLog],
-                    "M3"     : ["M_{3} (GeV)",  YesLog],
-                    "nVtx"   : ["N Vtx",   NoLog],
-                    "nVtxup"   : ["N Vtx",   NoLog],
-                    "nVtxdo"   : ["N Vtx",   NoLog],
-                    }
-
-phoselhistograms = {"photonEt"      : ["Photon Et (GeV)"  , NoLog],     
-                    "photonPhi"     : ["Photon Phi (GeV)" , NoLog],    
-                    "photonEta"     : ["Photon Eta (GeV)" , NoLog],    
-                    # "dRPhotonLepton" : ["dR(Photon,Lepton)", NoLog],
-                    # "dRPhotonJet"    : ["dR(Photon,Jet)"   , NoLog],   
-                    # "M3"             : ["M_{3} (GeV)"         , NoLog],            
-                    }
-
+CMS_lumi.channelText = _channelText
+CMS_lumi.writeChannelText = True
+CMS_lumi.writeExtraText = True
 
 
 H = 600;
@@ -80,6 +151,59 @@ canvas.SetTopMargin( T/H )
 canvas.SetBottomMargin( B/H )
 canvas.SetTickx(0)
 
+
+canvasRatio = TCanvas('c1Ratio','c1Ratio',W,H)
+# references for T, B, L, R                                                                                                             
+T = 0.08*H
+B = 0.12*H
+L = 0.12*W
+R = 0.04*W
+canvasRatio.SetFillColor(0)
+canvasRatio.SetBorderMode(0)
+canvasRatio.SetFrameFillStyle(0)
+canvasRatio.SetFrameBorderMode(0)
+canvasRatio.SetLeftMargin( L/W )
+canvasRatio.SetRightMargin( R/W )
+canvasRatio.SetTopMargin( T/H )
+canvasRatio.SetBottomMargin( B/H )
+canvasRatio.SetTickx(0)
+canvasRatio.SetTicky(0)
+pad1 = TPad("p1","p1",0,padRatio-padOverlap,1,1)
+pad2 = TPad("p2","p2",0,0,1,padRatio+padOverlap)
+pad1.SetLeftMargin( L/W )
+pad1.SetRightMargin( R/W )
+pad1.SetTopMargin( T/H/(1-padRatio+padOverlap) )
+pad1.SetBottomMargin( (padOverlap+padGap)/(1-padRatio+padOverlap) )
+pad2.SetLeftMargin( L/W )
+pad2.SetRightMargin( R/W )
+pad2.SetTopMargin( (padOverlap)/(padRatio+padOverlap) )
+pad2.SetBottomMargin( B/H/(padRatio+padOverlap) )
+
+pad1.SetFillColor(0)
+pad1.SetBorderMode(0)
+pad1.SetFrameFillStyle(0)
+pad1.SetFrameBorderMode(0)
+pad1.SetTickx(0)
+pad1.SetTicky(0)
+
+pad2.SetFillColor(0)
+pad2.SetFillStyle(4000)
+pad2.SetBorderMode(0)
+pad2.SetFrameFillStyle(0)
+pad2.SetFrameBorderMode(0)
+pad2.SetTickx(0)
+pad2.SetTicky(0)
+
+SetOwnership(canvas, False)
+SetOwnership(canvasRatio, False)
+SetOwnership(pad1, False)
+SetOwnership(pad2, False)
+
+canvasRatio.cd()
+pad1.Draw()
+pad2.Draw()
+
+
 canvas.cd()
 
 legList = {'TTGamma': [kAzure+3, 't#bar{t}+#gamma'],
@@ -89,7 +213,7 @@ legList = {'TTGamma': [kAzure+3, 't#bar{t}+#gamma'],
           'SingleTop': [kMagenta, 'Single t'],
           'WJets': [kGreen -3, 'W+jets'],
           'ZJets': [kGreen -3, 'Z+jets'],
-          'QCD': [kYellow, 'Multijet'],
+          'QCDMu': [kYellow, 'Multijet'],
           }
 
 mcList = {'TTGamma': [kOrange],
@@ -97,14 +221,22 @@ mcList = {'TTGamma': [kOrange],
           'TTV': [kRed-7],
           'SingleTop': [kOrange-3],
           'WGamma': [kBlue-4],
-          'ZGamma': [kBlue-4],
+          'ZGamma': [kBlue-2],
           'WJets': [kCyan-3],
-          'ZJets': [kCyan-3],
-
-          'QCD': [kGreen+3],
+          'ZJets': [kCyan-5],
+          'QCDMu': [kGreen+3],
           }
 
-legend = TLegend(0.71, 1-T/H-0.01 - 0.06*(len(legList)+1), 1-R/W-0.01, 1-(T/H)-0.01)
+
+legendHeightPer = 0.04
+
+legend = TLegend(0.71, 1-T/H-0.01 - legendHeightPer*(len(legList)+1), 1-R/W-0.01, 1-(T/H)-0.01)
+
+legendR = TLegend(0.71, 0.99 - (T/H)/(1.-padRatio+padOverlap) - legendHeightPer/(1.-padRatio+padOverlap)*(len(legList)+1), 0.99-(R/W), 0.99-(T/H)/(1.-padRatio+padOverlap))
+legendR.SetBorderSize(0)
+legendR.SetFillColor(0)
+
+
 
 legend.SetBorderSize(0)
 legend.SetFillColor(0)
@@ -114,62 +246,134 @@ legList = stackList[:]
 legList.reverse()
 dataHist = _file.Get("DataMu/presel_%s_DataMu"%(histName))
 legend.AddEntry(dataHist, "Data", 'pe')
+legendR.AddEntry(dataHist, "Data", 'pe')
 
 for sample in legList:
     hist = _file.Get("%s/presel_%s_%s"%(sample,histName,sample))
     hist.SetFillColor(mcList[sample][0])
     hist.SetLineColor(mcList[sample][0])
     legend.AddEntry(hist,sample,'f')
+    legendR.AddEntry(hist,sample,'f')
+
+
+TGaxis.SetMaxDigits(3)
+
+def drawHist(histName,plotInfo, plotDirectory, _file):
+
+    canvas.cd()
+    canvas.ResetDrawn()
+    stack = THStack(histName,histName)
+    for sample in stackList:
+#        print sample
+        hist = _file.Get("%s/%s_%s"%(sample,histName,sample)).Clone(sample)
+        hist.SetFillColor(mcList[sample][0])
+        hist.SetLineColor(mcList[sample][0])
+        hist.Rebin(plotInfo[2])
+        stack.Add(hist)
+    dataHist = _file.Get("DataMu/%s_DataMu"%(histName))
+    dataHist.Rebin(plotInfo[2])
+
+
+    #histograms list has flag whether it's log or not
+    canvas.SetLogy(plotInfo[-1])
+
+    stack.SetMaximum(1.35*max(dataHist.GetMaximum(),stack.GetMaximum()))
+    stack.Draw("hist")
+    # histograms list has x-axis title
+    stack.GetHistogram().GetXaxis().SetTitle(plotInfo[0])
+    if not -1 in plotInfo[3]:
+        stack.GetHistogram().GetXaxis().SetRangeUser(plotInfo[3][0],plotInfo[3][1])
+        dataHist.GetXaxis().SetRangeUser(plotInfo[3][0],plotInfo[3][1])
+    stack.GetHistogram().GetYaxis().SetTitle(plotInfo[1])
+
+    dataHist.Draw("e,X0,same")
+    legend.Draw("same")
+
+    CMS_lumi.channelText = _channelText+plotInfo[4]
+    CMS_lumi.CMS_lumi(canvas, 4, 11)
+    canvas.Print("%s/%s.pdf"%(plotDirectory,histName),".pdf")
+
+    ratio = dataHist.Clone("temp")
+    ratio.Divide(stack.GetStack().Last())
+ 
+ 
+    pad1.Clear()
+    pad2.Clear()
+
+    canvasRatio.cd()
+    canvasRatio.ResetDrawn()
+
+    pad1.Clear()
+    pad2.Clear()
+
+
+    pad1.cd()
+
+    pad1.SetLogy(plotInfo[-1])
+    stack.Draw('HIST')
+#    pad1.Update()
+    y2 = pad1.GetY2()
+    
+    stack.SetMinimum(1)
+#    pad1.Update()
+    stack.GetXaxis().SetTitle('')
+#    stack.GetYaxis().SetTitle(dataHist.GetYaxis().GetTitle())
+
+    stack.SetTitle('')
+    stack.GetXaxis().SetLabelSize(0)
+    stack.GetYaxis().SetLabelSize(gStyle.GetLabelSize()/(1.-padRatio+padOverlap))
+    stack.GetYaxis().SetTitleSize(gStyle.GetTitleSize()/(1.-padRatio+padOverlap))
+    stack.GetYaxis().SetTitleOffset(gStyle.GetTitleYOffset()*(1.-padRatio+padOverlap))
+    dataHist.Draw('E,X0,SAME')
+    legendR.Draw()
+    ratio.SetTitle('')
+            
+    ratio.GetXaxis().SetLabelSize(gStyle.GetLabelSize()/(padRatio+padOverlap))
+    ratio.GetYaxis().SetLabelSize(gStyle.GetLabelSize()/(padRatio+padOverlap))
+    ratio.GetXaxis().SetTitleSize(gStyle.GetTitleSize()/(padRatio+padOverlap))
+    ratio.GetYaxis().SetTitleSize(gStyle.GetTitleSize()/(padRatio+padOverlap))
+    ratio.GetYaxis().SetTitleOffset(gStyle.GetTitleYOffset()*(padRatio+padOverlap-padGap))
+    ratio.GetYaxis().SetRangeUser(0.75,1.25)
+    ratio.GetYaxis().SetNdivisions(504)
+    ratio.GetXaxis().SetTitle(plotInfo[0])
+    ratio.GetYaxis().SetTitle("Data/MC")
+
+    pad2.cd()
+    ratio.SetMarkerStyle(dataHist.GetMarkerStyle())
+    ratio.SetMarkerSize(dataHist.GetMarkerSize())
+    ratio.SetLineColor(dataHist.GetLineColor())
+    ratio.SetLineWidth(dataHist.GetLineWidth())
+
+    oneLine = TF1("oneline","1",-9e9,9e9)
+    oneLine.SetLineColor(kBlack)
+    oneLine.SetLineWidth(1)
+    oneLine.SetLineStyle(2)
+
+    ratio.Draw('e,x0')
+    oneLine.Draw("same")
+
+#    pad2.Update()
+    CMS_lumi.CMS_lumi(pad1, 4, 11)
+    canvasRatio.Update()
+    canvasRatio.RedrawAxis()
+
+    canvasRatio.SaveAs("%s/%s_ratio.pdf"%(plotDirectory,histName))
+    canvasRatio.SetLogy(0)
+
+
+
+
 
 for histName in preselhistograms:
-    stack = THStack("presel_%s"%histName,"presel_%s"%histName)
-
-    for sample in stackList:
-        hist = _file.Get("%s/presel_%s_%s"%(sample,histName,sample)).Clone(sample)
-        
-        # print hist
-        # print hist.Integral()
-        hist.SetFillColor(mcList[sample][0])
-        hist.SetLineColor(mcList[sample][0])
-        stack.Add(hist)
-        # print stack.GetStack().Last().Integral()
-
-
-    dataHist = _file.Get("DataMu/presel_%s_DataMu"%(histName))
-    
-    print dataHist
-    canvas.SetLogy(preselhistograms[histName][-1])
-
-    stack.SetMaximum(1.05*max(dataHist.GetMaximum(),stack.GetMaximum()))
-    stack.Draw("HIST")
-    stack.GetHistogram().GetXaxis().SetTitle(preselhistograms[histName][0])
-    dataHist.Draw("e,X0,same")
-    legend.Draw("same")
-    canvas.SaveAs("plots/presel_mu_%s.pdf"%histName)
-    canvas.SetLogy(0)
-
-
+    drawHist("presel_%s"%histName,preselhistograms[histName],plotDirectory,_file)
 
 for histName in phoselhistograms:
-    stack = THStack("phosel_%s"%histName,"phosel_%s"%histName)
-    for sample in stackList:
-        hist = _file.Get("%s/phosel_%s_%s"%(sample,histName,sample)).Clone(sample)
-        # print hist
-        # print hist.Integral()
-        hist.SetFillColor(mcList[sample][0])
-        hist.SetLineColor(mcList[sample][0])
-        stack.Add(hist)
-        # print stack.GetStack().Last().Integral()
-    dataHist = _file.Get("DataMu/phosel_%s_DataMu"%(histName))
+    drawHist("phosel_%s"%histName,phoselhistograms[histName],plotDirectory,_file)
 
-    print dataHist
-    canvas.SetLogy(phoselhistograms[histName][-1])
 
-    stack.SetMaximum(1.05*max(dataHist.GetMaximum(),stack.GetMaximum()))
-    stack.Draw("hist")
-    stack.GetHistogram().GetXaxis().SetTitle(phoselhistograms[histName][0])
-    dataHist.Draw("e,X0,same")
-    legend.Draw("same")
+# _file2  = TFile("histograms/mu/testBtaghists.root")
 
-    canvas.SaveAs("plots/phosel_mu_%s.pdf"%histName)
-
+# drawHist("njets1Tag",      ["NJets", "Events", 1, [-1,10], ", =1 b-tag"    , NoLog], plotDirectory, _file2)
+# drawHist("njets2Tag",      ["NJets", "Events", 1, [-1,10], ", #geq2 b-tags", NoLog], plotDirectory, _file2)
+# drawHist("phoselnjets1Tag",["NJets", "Events", 1, [-1,10], ", =1 b-tag"    , NoLog], plotDirectory, _file2)
+# drawHist("phoselnjets2Tag",["NJets", "Events", 1, [-1,10], ", #geq2 b-tags", NoLog], plotDirectory, _file2)
