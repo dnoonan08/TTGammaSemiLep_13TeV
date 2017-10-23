4e988ad24f992f2119785986aeca15ae1712e643
Danny Noonan <dnoonan@cern.ch>
2017-10-23 14:20:20 -0500

    update electron condor scripts

--- runSkimsAndAnalysisNtuples_ele.sh
+++ runSkimsAndAnalysisNtuples_ele.sh
@@ -8,11 +8,16 @@ else
 	echo "Running In Batch"
 	cd ${_CONDOR_SCRATCH_DIR}
 	echo ${_CONDOR_SCRATCH_DIR}
+
+	echo "xrdcp root://cmseos.fnal.gov//store/user/"${USER}"/CMSSW_8_0_26_patch1.tgz ."
+	xrdcp root://cmseos.fnal.gov//store/user/${USER}/CMSSW_8_0_26_patch1.tgz .
 	echo "tar -xvf CMSSW_8_0_26_patch1.tgz"
 	tar -xzf CMSSW_8_0_26_patch1.tgz
 	cd CMSSW_8_0_26_patch1/src/
 	source /cvmfs/cms.cern.ch/cmsset_default.sh
 	cd  TTGammaSemiLep_13TeV/
+	echo "xrdcp -r root://cmseos.fnal.gov//store/user/dnoonan/DataPUfiles_2016 ."
+	xrdcp -r root://cmseos.fnal.gov//store/user/dnoonan/DataPUfiles_2016 .
 fi
 
 eval `scramv1 runtime -sh`
@@ -20,46 +25,33 @@ eval `scramv1 runtime -sh`
 
 
 
-outputdir="root://cmseos.fnal.gov//store/user/dnoonan/13TeV_"
+outputdir="root://cmseos.fnal.gov//store/user/lpctop/TTGamma/13TeV_"
 
 
 files=("TTGamma_SingleLeptFromTbar_" \
 "TTGamma_SingleLeptFromT_" \
 "TTGamma_Dilepton_" \
 "TTGamma_Hadronic_" \
-"TTbar_" \
+"TTbarPowheg_" \
+"TTbarMadgraph_SingleLeptFromT_" \
+"TTbarMadgraph_SingleLeptFromTbar_" \
+"TTbarMadgraph_Dilepton_" \
 "W1jets_" \
 "W2jets_" \
 "W3jets_" \
 "W4jets_" \
-"DYjets_" \
+"DYjetsM10to50_" \
+"DYjetsM50_" \
 "ST_s-channel_" \
 "ST_t-channel_" \
 "ST_tbar-channel_" \
 "ST_tW-channel_" \
 "ST_tbarW-channel_" \
-"TTW_" \
-"TTZ_" \
+"TTWtoQQ_" \
+"TTWtoLNu_" \
+"TTZtoLL_" \
 "WGamma_" \
 "ZGamma_" \
-"QCD_20to30EM_" \
-"QCD_30to50EM_" \
-"QCD_50to80EM_" \
-"QCD_80to120EM_" \
-"QCD_120to170EM_" \
-"QCD_170to300EM_" \
-"QCD_300toInfEM_" \
-"QCD_20to30Mu_" \
-"QCD_30to50Mu_" \
-"QCD_50to80Mu_" \
-"QCD_80to120Mu_" \
-"QCD_120to170Mu_" \
-"QCD_170to300Mu_" \
-"QCD_300to470Mu_" \
-"QCD_470to600Mu_" \
-"QCD_600to800Mu_" \
-"QCD_800to1000Mu_" \
-"QCD_1000toInfMu_" \
 "Data_SingleEle_b_" \
 "Data_SingleEle_c_" \
 "Data_SingleEle_d_" \
@@ -69,112 +61,92 @@ files=("TTGamma_SingleLeptFromTbar_" \
 "Data_SingleEle_h_")
 
 
-inputfiles=("root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/ttgamma_SingleLeptFromTbar.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/ttgamma_semileptfromT.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/ttgamma_dilept.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/ttgamma_hadronic.root" \
-"root://cmseos.fnal.gov//store/user/yumiceva/ntuples_2016/TTbar.root" \
-"root://cmseos.fnal.gov//store/user/dnoonan/13TeV_ggNTuples/W1jets.root" \
-"root://cmseos.fnal.gov//store/user/dnoonan/13TeV_ggNTuples/W2jets.root" \
-"root://cmseos.fnal.gov//store/user/dnoonan/13TeV_ggNTuples/W3jets.root" \
-"root://cmseos.fnal.gov//store/user/dnoonan/13TeV_ggNTuples/W4jets.root" \
-"root://cmseos.fnal.gov//store/user/yumiceva/ntuples_2016/ntuple_DYjets.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/ST_s.root" \
-"root://cmseos.fnal.gov//store/user/yumiceva/ntuples_2016/ST_t_top.root" \
-"root://cmseos.fnal.gov//store/user/yumiceva/ntuples_2016/ST_t_bar.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/ST_tW_top.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/ST_t_tWbar.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/TTWJets.root" \
-"root://cmseos.fnal.gov//store/user/yumiceva/ntuples_2016/TTZ.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/WGToLNuG_aMCatNLO_summer16.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/ZGTo2LG_aMCatNLO_summer16.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_20to30EM.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_30to50EM.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_50to80EM.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_80to120EM.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_120to170EM.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_170to300EM.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_300toInfEM.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_20to30Mu.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_30to50Mu.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_50to80Mu.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_80to120Mu.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_120to170Mu.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_170to300Mu.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_300to470Mu.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_470to600Mu.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_600to800Mu.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_800to1000Mu.root" \
-"root://cmseos.fnal.gov//store/user/troy2012/ntuples_2016/QCD_1000toInfMu.root" \
-"root://eoscms.cern.ch//store/caf/user/dnoonan/job_SingleEle_Run2016B_FebReminiAOD.root" \
-"root://eoscms.cern.ch//store/caf/user/dnoonan/job_SingleEle_Run2016C_FebReminiAOD.root" \
-"root://eoscms.cern.ch//store/caf/user/dnoonan/job_SingleEle_Run2016D_FebReminiAOD.root" \
-"root://eoscms.cern.ch//store/caf/user/dnoonan/job_SingleEle_Run2016E_FebReminiAOD.root" \
-"root://eoscms.cern.ch//store/caf/user/dnoonan/job_SingleEle_Run2016F_FebReminiAOD1.root root://eoscms.cern.ch//store/caf/user/dnoonan/job_SingleEle_Run2016F_FebReminiAOD2.root" \
-"root://eoscms.cern.ch//store/caf/user/dnoonan/job_SingleEle_Run2016G_FebReminiAOD.root" \
-"root://eoscms.cern.ch//store/caf/user/dnoonan/job_SingleEle_Run2016H_FebReminiAODv2.root root://eoscms.cern.ch//store/caf/user/dnoonan/job_SingleEle_Run2016H_FebReminiAODv3.root")
+DannyEOS="root://cmseos.fnal.gov//store/user/dnoonan/13TeV_ggNTuples/V08_00_26_07/"
+GGNtupleGroupEOSMC="root://cmseos.fnal.gov//store/user/lpcggntuples/ggNtuples/13TeV/mc/V08_00_26_07/"
+TitasEOS="root://cmseos.fnal.gov//store/user/troy2012/13TeV_ggNTuples/V08_00_26_07/"
+GGNtupleGroupEOSData="root://cmseos.fnal.gov//store/user/lpcggntuples/ggNtuples/13TeV/data/V08_00_26_07/"
+
+inputfiles=($DannyEOS"TTGamma_SingleLeptFromTbar_TuneCUETP8M2T4_13TeV-amcatnlo-pythia8.root" \
+$DannyEOS"TTGamma_SingleLeptFromT_TuneCUETP8M2T4_13TeV-amcatnlo-pythia8.root" \
+$DannyEOS"TTGamma_Dilept_TuneCUETP8M2T4_13TeV-amcatnlo-pythia8.root" \
+$DannyEOS"TTGamma_Hadronic_TuneCUETP8M2T4_13TeV-amcatnlo-pythia8.root" \
+$GGNtupleGroupEOSMC"TT_TuneCUETP8M2T4_13TeV-powheg-pythia8_1of4.root "$GGNtupleGroupEOSMC"TT_TuneCUETP8M2T4_13TeV-powheg-pythia8_2of4.root "$GGNtupleGroupEOSMC"TT_TuneCUETP8M2T4_13TeV-powheg-pythia8_3of4.root "$GGNtupleGroupEOSMC"TT_TuneCUETP8M2T4_13TeV-powheg-pythia8_4of4.root" \
+$DannyEOS"TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root" \
+$DannyEOS"TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root" \
+$DannyEOS"TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root" \
+$GGNtupleGroupEOSMC"W1JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root" \
+$GGNtupleGroupEOSMC"W2JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root" \
+$GGNtupleGroupEOSMC"W3JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root" \
+$GGNtupleGroupEOSMC"W4JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root" \
+$GGNtupleGroupEOSMC"DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_1.root "$GGNtupleGroupEOSMC"DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_2.root" \
+$TitasEOS"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_2_1of2.root "$TitasEOS"DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_2_1of2.root"
+$DannyEOS"ST_s-channel_4f_InclusiveDecays_13TeV-amcatnlo-pythia8.root" \
+$DannyEOS"ST_t-channel_top_4f_inclusiveDecays_13TeV-powhegV2-madspin-pythia8_TuneCUETP8M1.root" \
+$DannyEOS"ST_t-channel_antitop_4f_inclusiveDecays_13TeV-powhegV2-madspin-pythia8_TuneCUETP8M1.root" \
+$DannyEOS"ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1.root" \
+$DannyEOS"ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1.root" \
+$GGNtupleGroupEOSMC"TTWJetsToQQ_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8.root" \
+$GGNtupleGroupEOSMC"TTWJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8.root" \
+$GGNtupleGroupEOSMC"TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8.root" \
+$DannyEOS"WGToLNuG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8.root" \
+$DannyEOS"ZGTo2LG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8.root" \
+$GGNtupleGroupEOSData"job_SingleMu_Run2016B_FebReminiAOD.root" \
+$GGNtupleGroupEOSData"job_SingleMu_Run2016C_FebReminiAOD.root" \
+$GGNtupleGroupEOSData"job_SingleMu_Run2016D_FebReminiAOD.root" \
+$GGNtupleGroupEOSData"job_SingleMu_Run2016E_FebReminiAOD.root" \
+$GGNtupleGroupEOSData"job_SingleMu_Run2016F_FebReminiAOD.root" \
+$GGNtupleGroupEOSData"job_SingleMu_Run2016G_FebReminiAOD.root" \
+$GGNtupleGroupEOSData"job_SingleMu_Run2016H_FebReminiAODv2.root "$GGNtupleGroupEOSData"job_SingleMu_Run2016H_FebReminiAODv3.root")
 
 
 
 
-sampleType=("TTGamma_SingleLeptFromTbar " \
-"TTGamma_SingleLeptFromT    " \
-"TTGamma_Dilepton           " \
-"TTGamma_Hadronic           " \
-"TTbarPowheg                " \
-"W1jets                     " \
-"W2jets                     " \
-"W3jets                     " \
-"W4jets                     " \
-"DYjets                     " \
-"ST_s-channel               " \
-"ST_t-channel               " \
-"ST_tbar-channel            " \
-"ST_tW-channel              " \
-"ST_tbarW-channel           " \
-"TTW                        " \
-"TTZ                        " \
-"WGamma                     " \
-"ZGamma                     " \
-"QCD_Pt20to30_EM            " \
-"QCD_Pt30to50_EM            " \
-"QCD_Pt50to80_EM            " \
-"QCD_Pt80to120_EM           " \
-"QCD_Pt120to170_EM          " \
-"QCD_Pt170to300_EM          " \
-"QCD_Pt300toInf_EM          " \
-"QCD_Pt20to30_Mu            " \
-"QCD_Pt30to50_Mu            " \
-"QCD_Pt50to80_Mu            " \
-"QCD_Pt80to120_Mu           " \
-"QCD_Pt120to170_Mu          " \
-"QCD_Pt170to300_Mu          " \
-"QCD_Pt300to470_Mu          " \
-"QCD_Pt470to600_Mu          " \
-"QCD_Pt600to800_Mu          " \
-"QCD_Pt800to1000_Mu         " \
-"QCD_Pt1000toInf_Mu         " \
-"Data                       " \
-"Data                       " \
-"Data                       " \
-"Data                       " \
-"Data                       " \
-"Data                       " \
-"Data                       ")
+
+sampleType=("TTGamma_SingleLeptFromTbar" \
+"TTGamma_SingleLeptFromT" \
+"TTGamma_Dilepton" \
+"TTGamma_Hadronic" \
+"TTbarPowheg" \
+"TTbarMadgraph_SingleLeptFromT" \
+"TTbarMadgraph_SingleLeptFromTbar" \
+"TTbarMadgraph_Dilepton" \
+"W1jets" \
+"W2jets" \
+"W3jets" \
+"W4jets" \
+"DYjetsM10to50" \
+"DYjetsM50" \
+"ST_s-channel" \
+"ST_t-channel" \
+"ST_tbar-channel" \
+"ST_tW-channel" \
+"ST_tbarW-channel" \
+"TTWtoQQ" \
+"TTWtoLNu" \
+"TTZtoLL" \
+"WGamma" \
+"ZGamma" \
+"Data_SingleEle_b" \
+"Data_SingleEle_c" \
+"Data_SingleEle_d" \
+"Data_SingleEle_e" \
+"Data_SingleEle_f" \
+"Data_SingleEle_g" \
+"Data_SingleEle_h")
 
 
 
 echo "AnalysisNtuple/makeSkim ele ${files[job]}skim.root ${inputfiles[job]}"
 AnalysisNtuple/makeSkim ele ${files[job]}skim.root ${inputfiles[job]}
 
-echo "AnalysisNtuple/makeAnalysisNtuple ${sampleType[job]} ${files[job]}AnalysisNtuple.root ${files[job]}skim.root"
-AnalysisNtuple/makeAnalysisNtuple ${sampleType[job]} ${files[job]}AnalysisNtuple.root ${files[job]}skim.root
+echo "AnalysisNtuple/makeAnalysisNtuple ${sampleType[job]} . ${files[job]}skim.root"
+AnalysisNtuple/makeAnalysisNtuple ${sampleType[job]} . ${files[job]}skim.root
 
 
-echo "xrdcp -f ${files[job]}skim.root ${outputdir}skims/electrons/"
-xrdcp -f ${files[job]}skim.root ${outputdir}skims/electrons/
+echo "xrdcp -f ${files[job]}skim.root ${outputdir}skims/electrons/V08_00_26_07/"
+xrdcp -f ${files[job]}skim.root ${outputdir}skims/electrons/V08_00_26_07/
 
-echo "xrdcp -f ${files[job]}AnalysisNtuple.root ${outputdir}AnalysisNtuples/electrons/"
-xrdcp -f ${files[job]}AnalysisNtuple.root ${outputdir}AnalysisNtuples/electrons/
+echo "xrdcp -f ${files[job]}AnalysisNtuple.root ${outputdir}AnalysisNtuples/electrons/V08_00_26_07/"
+xrdcp -f ${files[job]}AnalysisNtuple.root ${outputdir}AnalysisNtuples/electrons/V08_00_26_07/
 
 
