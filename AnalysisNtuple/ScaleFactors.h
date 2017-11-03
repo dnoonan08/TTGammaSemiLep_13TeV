e999b5a6f414e59f220e22028899a14a9e548905
titasroy89 <titasroy89@gmail.com>
2017-11-03 15:06:28 -0500

    Skipping data for photon category and adding DataEle options in allowedsamples

--- AnalysisNtuple/ScaleFactors.h
+++ AnalysisNtuple/ScaleFactors.h
@@ -248,11 +248,17 @@ double getEvtWeight(string sampleType){
 
 
 
-const std::string allowedSampleTypes[53] = {"Data",
-											"Data_SingleMu_b",
-											"Data_SingleMu_c",
-											"Data_SingleMu_d",
-											"Data_SingleMu_e",
+const std::string allowedSampleTypes[59] = {"Data_SingleEle_b",
+					    "Data_SingleEle_c",
+					    "Data_SingleEle_d",
+					    "Data_SingleEle_e",
+					    "Data_SingleEle_f",
+					    "Data_SingleEle_g",
+					    "Data_SingleEle_h"
+					    "Data_SingleMu_b",
+					    "Data_SingleMu_c",
+					    "Data_SingleMu_d",
+					    "Data_SingleMu_e",
 											"Data_SingleMu_f",
 											"Data_SingleMu_g",
 											"Data_SingleMu_h",
