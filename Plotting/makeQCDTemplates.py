1d71849730cacde7b121478a767058ded814d34e
titasroy89 <titasroy89@gmail.com>
2018-02-16 15:43:33 -0600

    small fixed is plotting and QCD template making

--- Plotting/makeQCDTemplates.py
+++ Plotting/makeQCDTemplates.py
@@ -10,7 +10,7 @@ parser.add_option("--Tight","--tight", dest="isTightSelection", default=False,ac
                      help="Use 4j2t selection" )
 parser.add_option("--Loose","--loose", dest="isLooseSelection", default=False,action="store_true",
                      help="Use 2j0t selection" )
-parser.add_option("-f","--file",dest="fileDir",default="histograms/mu/qcdhistsCR/",
+parser.add_option("-f","--file",dest="fileDir",default="histograms/ele/qcdhistsCR",
                      help="histogram file direcotry")
 
 (options, args) = parser.parse_args()
@@ -61,29 +61,34 @@ if finalState=='Mu':
 
 
 gROOT.SetBatch(True)
+_file["Data%s"%finalState].cd("Data%s"%finalState)
 
-keylist = _file["Data%s"%finalState].GetListOfKeys()
+keylist = gDirectory.GetListOfKeys()
+	
+
 
 histoList = {}
-
+print stackList
 #key = _file.FindKey("Data%s"%finalState)
 ##print key
 #list2 = key.ReadObj().GetListOfKeys()
-
+#print keylist
 for key in keylist:
-
+    
     name = key.GetName()
     split = name.split('_')
     nameKey = split[0]
+    print split, nameKey, split[1:-1]
 
     for n in split[1:-1]: nameKey += "_%s"%n
 
     hName = "%s_QCD_DD"%(nameKey)
-
+    print hName
     histoList[nameKey] = key.ReadObj().Clone(hName)
     histoList[nameKey].SetNameTitle(hName,hName)
 
     for sample in stackList:
+	print "%s_%s"%(nameKey,sample)
         tempHist = _file[sample].Get("%s_%s"%(nameKey,sample))
         histoList[nameKey].Add(tempHist,-1)
         
