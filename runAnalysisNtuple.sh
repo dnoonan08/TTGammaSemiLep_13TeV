6ad7639ae2b269b68800349dbe19d19a1064f087
Danny Noonan <dnoonan@cern.ch>
2020-03-02 18:20:16 -0600

    update to allow splitting a single sample into different jobs

--- runAnalysisNtuple.sh
+++ runAnalysisNtuple.sh
@@ -2,6 +2,8 @@
 
 sample=$1
 year=$2
+job=$3
+nJobs=$4
 
 if [ -z ${_CONDOR_SCRATCH_DIR} ] ; then 
     echo "Running Interactively" ; 
@@ -20,23 +22,33 @@ else
     mkdir AnalysisNtuple
     mv makeAnalysisNtuple AnalysisNtuple/makeAnalysisNtuple
     echo "Done With Setup" ;
-
 fi
 
+
+echo ${sample}
+echo ${year}
+echo ${job}
+echo ${nJobs}
 
 outputdir="root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/"
 
 source sampleList_${year}.sh
 varname=${sample}_${year}
 
-echo "./AnalysisNtuple/makeAnalysisNtuple ${year} ${sample} . ${!varname}"
-./AnalysisNtuple/makeAnalysisNtuple ${year} ${sample} . ${!varname}
+if [ -z ${job} ] ; then
+    echo "./AnalysisNtuple/makeAnalysisNtuple ${year} ${sample} . ${!varname}"
+    ./AnalysisNtuple/makeAnalysisNtuple ${year} ${sample} . ${!varname};
+else
+    echo "./AnalysisNtuple/makeAnalysisNtuple ${year} ${sample} ${job}of${nJobs} . ${!varname}"
+    ./AnalysisNtuple/makeAnalysisNtuple ${year} ${sample} ${job}of${nJobs} . ${!varname};
+fi
+
 
 if [ -z ${_CONDOR_SCRATCH_DIR} ] ; then
     echo "Finished" ;
 else
-    xrdcp -f ${sample}_${year}_AnalysisNtuple.root ${outputdir}/${year}
+    xrdcp -f ${sample}_${year}_AnalysisNtuple*.root ${outputdir}/${year}
     echo "Finished, Cleaning up"
     rm *root
-    rm RequiredScaleFactors.tgz
+    rm RequiredScaleFactors.tgz;
 fi
